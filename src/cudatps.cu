4a6151d68797aeba82f39eaca96f8995694bbf77
Thiago Nunes <gorobaum@gmail.com>
2015-03-23 08:48:23 -0300

    Passing over all the vector

--- src/cudatps.cu
+++ src/cudatps.cu
@@ -5,10 +5,11 @@
 #define MAXTHREADPBLOCK 1024
 
 // Kernel definition
-__global__ void tpsCuda(float* cudaImageCoord)
+__global__ void tpsCuda(float* cudaImageCoord, int dimension)
 {
-  int x = threadIdx.x;
-	cudaImageCoord[x] = 1.0;
+  int x = blockDim.x*blockIdx.x + threadIdx.x;
+  int y = blockDim.y*blockIdx.y + threadIdx.y;
+	cudaImageCoord[x+dimension*y] = blockIdx.y;
 }
 
 void tps::CudaTPS::run() {
@@ -17,28 +18,29 @@ void tps::CudaTPS::run() {
 	allocResources();
 	allocCudaResources();
 
-	dim3 threadsPerBlock(256, 256);
-	std::cout << "numBlocks[0] = " << dimensions[0] / threadsPerBlock.x << std::endl;
-	std::cout << "numBlocks[1] = " << dimensions[1] / threadsPerBlock.y << std::endl;
+	dim3 threadsPerBlock(16, 16);
 	dim3 numBlocks(dimensions[0] / threadsPerBlock.x, dimensions[1] / threadsPerBlock.y);
-	tpsCuda<<<1, threadsPerBlock>>>(cudaImageCoord);
+	std::cout << "numBlocks[0] = " << numBlocks.x << std::endl;
+	std::cout << "numBlocks[1] = " << numBlocks.y << std::endl;
+	tpsCuda<<<numBlocks, threadsPerBlock>>>(cudaImageCoord, dimensions[0]);
 
 	freeResources();
 	freeCudaResources();
 
-	cudaDeviceSynchronize();
-	std::cout << "imageCoord[0] = " << imageCoord[0] << std::endl;
+	for (int i = 0; i < 256*256; i++) {
+		std::cout << "imageCoord[i] = " << imageCoord[i] << std::endl;		
+	}
 }
 
 void tps::CudaTPS::allocResources() {
 	std::vector<int> dimensions = registredImage.getDimensions();
 
-	imageCoord = (float*)std::malloc(referenceKeypoints_.size()*dimensions[0]*dimensions[1]*sizeof(float*));
+	imageCoord = (float*)std::malloc(dimensions[0]*dimensions[1]*sizeof(float*));
 }
 
 void tps::CudaTPS::allocCudaResources() {
 	std::vector<int> dimensions = registredImage.getDimensions();
-	cudaMalloc(&cudaImageCoord, referenceKeypoints_.size()*dimensions[0]*dimensions[1]*sizeof(float));
+	cudaMalloc(&cudaImageCoord, dimensions[0]*dimensions[1]*sizeof(float));
 	// cudaMemcpy(cudaImageCoord, imageCoord, referenceKeypoints_.size()*dimensions[0]*dimensions[1]*sizeof(float), cudaMemcpyHostToDevice);
 }
 
@@ -48,5 +50,7 @@ void tps::CudaTPS::freeResources() {
 
 void tps::CudaTPS::freeCudaResources() {
 	std::vector<int> dimensions = registredImage.getDimensions();
-	cudaMemcpy(imageCoord, cudaImageCoord, referenceKeypoints_.size()*dimensions[0]*dimensions[1]*sizeof(float), cudaMemcpyDeviceToHost);
+	cudaMemcpy(imageCoord, cudaImageCoord, dimensions[0]*dimensions[1]*sizeof(float), cudaMemcpyDeviceToHost);
+
+	cudaDeviceSynchronize();
 }
