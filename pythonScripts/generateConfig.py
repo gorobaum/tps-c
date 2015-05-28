0e4f6a305b3876a5db1155bb86046fa5d7630a76
Thiago Nunes <gorobaum@gmail.com>
2015-05-28 10:55:48 -0300

    Refactoring the tests generation and cleaning process

--- pythonScripts/generateConfig.py
+++ pythonScripts/generateConfig.py
@@ -6,15 +6,16 @@ n = int(sys.argv[1])
 for i in range(2, n, 2):
   numFiles = numFiles + 1
   fileName = 'tps.config'+str(numFiles)
-  f = open(fileName,'w')
+  filepath = '../configurations/tps.config'+str(numFiles)
+  f = open(filepath,'w')
   f.write('images/grid-Tar.png\n')
   porcentage = i*0.01
   name = 'result-'+str(porcentage)+'\n'
   f.write(name)
   f.write(str(porcentage)+'\n')
 
-f = open('tps.configs', 'w')
+f = open('../tps.configs', 'w')
 f.write('images/grid-Ref.png\n')
 for x in range(1, numFiles+1, 1):
-  filename = 'confis/tps.config'+str(x)+'\n'
+  filename = 'configurations/tps.config'+str(x)+'\n'
   f.write(filename)
