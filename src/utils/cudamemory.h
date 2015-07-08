f2a968ba1e237e275eaecf5e1a53c34752420ce6
Thiago Nunes <gorobaum@gmail.com>
2015-07-08 09:22:23 -0300

    fixing typo on cudamemory.h

--- src/utils/cudamemory.h
+++ src/utils/cudamemory.h
@@ -29,9 +29,9 @@ public:
   float* getKeypointZ() { return keypointZ; };
   short* getTargetImage() { return targetImage; };
   short* getRegImage() { return regImage; };
-  void setSolX(std::vector<float> solution);
   void setSolY(std::vector<float> solution);
   void setSolZ(std::vector<float> solution);
+  void setSolX(std::vector<float> solution);
   std::vector<float> getHostSolX();
   std::vector<float> getHostSolY();
   std::vector<float> getHostSolZ();
